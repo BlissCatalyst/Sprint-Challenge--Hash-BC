1b3eea1e92b105388d988595f83c610b83502d17
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-09-06 11:06:34 -0600

    built valid proof function

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -38,7 +38,9 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    guess = f'{last_hash}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:6] == last_hash[-6:]
 
 
 if __name__ == '__main__':
